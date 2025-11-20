fb3e82b4432aeb12eb76af146527d2e64c7466ea
Rongmario <rongmario@proton.me>
2025-11-20 03:11:21 +0000

    OverrideOnly and InvokeOnly

---
+++ src/main/java/com/cleanroommc/anone/canon/InvokeOnly.java
@@ -0,0 +1,7 @@
+package com.cleanroommc.anone.canon;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+public @interface InvokeOnly { }
---
+++ src/main/java/com/cleanroommc/anone/canon/OverrideOnly.java
@@ -0,0 +1,9 @@
+package com.cleanroommc.anone.canon;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.METHOD)
+public @interface OverrideOnly { }
