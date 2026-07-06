9c9ee9c81c4107f6e0545f429e312e15b64e3f28
tttsaurus <72607439+tttsaurus@users.noreply.github.com>
2026-07-06 17:14:28 +0800

    Update Immutable & Unmodifiable Javadoc and relate these two

--- src/main/java/com/cleanroommc/anone/Immutable.java
+++ src/main/java/com/cleanroommc/anone/Immutable.java
@@ -6,6 +6,8 @@ import java.lang.annotation.*;
  * Marks the type as immutable.
  * This would mean that the annotated type strictly does not allow mutation to be applied.
  * This annotation doesn't ensure complete immutability when Reflection or any bytecode manipulation is being used.
+ * <p>
+ * It has stronger semantics than {@link Unmodifiable}.
  *
  * @see Singleton
  * @see Unmodifiable
--- src/main/java/com/cleanroommc/anone/Unmodifiable.java
+++ src/main/java/com/cleanroommc/anone/Unmodifiable.java
@@ -7,6 +7,8 @@ import java.lang.annotation.*;
  * This would mean that the annotated type would error or produce no effect
  * when any mutator methods such as {@link java.util.Collection#add(Object)} is called.
  * This annotation doesn't ensure the complete immutability as the object may still be able to be mutated in a different context.
+ * <p>
+ * It has weaker semantics than {@link Immutable}.
  *
  * @see Immutable
  *
