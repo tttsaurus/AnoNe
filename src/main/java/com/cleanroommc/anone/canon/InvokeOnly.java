5a69460c4c2c37ced5b78b4d8640369a224734f2
tttsaurus <72607439+tttsaurus@users.noreply.github.com>
2026-07-06 17:27:43 +0800

    Add InvokeOnly & OverrideOnly Javadoc

--- src/main/java/com/cleanroommc/anone/canon/InvokeOnly.java
+++ src/main/java/com/cleanroommc/anone/canon/InvokeOnly.java
@@ -2,6 +2,16 @@ package com.cleanroommc.anone.canon;
 
 import java.lang.annotation.*;
 
+/**
+ * It marks that the method is intended to be invoked, not overridden.
+ * It targets potential overriders.
+ * <p>
+ * See {@link InvokeOnly} for the opposite side.
+ *
+ * @see OverrideOnly
+ *
+ * @since 1.0.0
+ */
 @Documented
 @Target(ElementType.METHOD)
 public @interface InvokeOnly { }
--- src/main/java/com/cleanroommc/anone/canon/OverrideOnly.java
+++ src/main/java/com/cleanroommc/anone/canon/OverrideOnly.java
@@ -4,6 +4,16 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * It marks that the method is intended to be overridden, not called.
+ * It targets potential callers.
+ * <p>
+ * See {@link OverrideOnly} for the opposite side.
+ *
+ * @see InvokeOnly
+ *
+ * @since 1.0.0
+ */
 @Documented
 @Target(ElementType.METHOD)
 public @interface OverrideOnly { }
